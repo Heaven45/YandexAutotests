945405b2e612d9dae10a9730359a64154eb7e8e3
s.fedulov <s.fedulov@corp.vk.com>
2021-11-06 12:59:00 +0300

    Fix date bug

--- src/test/java/cloud/autotests/helpers/DateTimeHelper.java
+++ src/test/java/cloud/autotests/helpers/DateTimeHelper.java
@@ -7,13 +7,13 @@ public class DateTimeHelper {
     String[] months = {"января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября",
             "октября", "ноября", "декабря"};
 
-    String[] daysOfWeek = {"суббота", "воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница"};
+    String[] daysOfWeek = {"воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"};
 
     GregorianCalendar gcalendar = new GregorianCalendar();
 
     public String currentMonth = months[gcalendar.get(Calendar.MONTH)];
     public Integer currentDay = gcalendar.get(Calendar.DATE);
-    public String currentDayOfWeek = daysOfWeek[gcalendar.get(Calendar.DAY_OF_WEEK)];
+    public String currentDayOfWeek = daysOfWeek[gcalendar.get(Calendar.DAY_OF_WEEK) - 1];
     public Integer currentHour = gcalendar.get(Calendar.HOUR);
     public Integer currentMinute = gcalendar.get(Calendar.MINUTE);
 }
