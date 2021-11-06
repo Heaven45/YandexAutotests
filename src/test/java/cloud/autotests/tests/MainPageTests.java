4b32c5f6853726ded54e3018953f0e06717f176d
s.fedulov <s.fedulov@corp.vk.com>
2021-11-06 13:06:03 +0300

    Change class naming

--- src/test/java/cloud/autotests/tests/MainPageTests.java
+++ src/test/java/cloud/autotests/tests/MainPageTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class GeneratedTests extends TestBase {
+public class MainPageTests extends TestBase {
 
     YandexMainPage mainPage = new YandexMainPage();
 
